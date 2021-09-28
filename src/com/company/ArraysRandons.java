7b1c3aeb095c92d07f4d81c7eaa1ed442095e0c4
robson <robson.flavio20@outlook.com>
2021-09-27 21:10:05 -0300

    Todas as exigencias foram atendidas. Foi implementado um menu de seleação e métodos com a intençãod de facilidar a leitura. Ainda será necessarios alguns ajustes.

---
+++ src/com/company/ArraysRandons.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class ArraysRandons {
+    public String[] nomes(){
+        String[] nm={"Alberto","Juvenal","Carlos","Roberto","Pedro","José","fim"};
+        return nm;
+    }
+
+    public String[] datasDeInclusao(){
+        String[] dt={"19/09/2021","20/09/2021","15/09/2021","20/09/2021","20/09/2021","21/09/2021"};
+        return dt;
+    }
+
+    public String[] datasDeAtendimento(){
+        String[] dt={"22/09/2021","24/09/2021","21/09/2021","21/09/2021","24/09/2021","25/09/2021"};
+        return dt;
+    }
+
+    public String[] email(){
+        String[] emails={"daviiagonelsondaluz..daviiagonelsondaluz@termakui.com.br"
+                ,"ppietraraimundamarcelaribeiro@gmalato.com.br"
+                ,"sergioguilhermecaldeira-94@ambev.com.br"
+                ,"reginapatriciacristianemonteiro-77@saojose.biz"
+                ,"renatomariorezende-94@petrobrais.com.br"
+                ,"gabrielnicolasthiagojesus_@kframe.com.br"};
+        return emails;
+    }
+
+    public String[] telefone(){
+        String[] fone={"(79) 98511-0814","(21) 99967-8033","(85) 98825-3580","(95) 98873-0442","(82) 99241-5302","(63) 98193-3301"};
+        return fone;
+    }
+
+    public String[] descricao(){
+        String[] desc={"O seu problema não é o problema. A sua reação é o problema"
+                ,"Só adie para amanhã o que você esta disposto a morrer tendo deixado desfeito."
+                ,"Não é a linguagem de programação que define o programador, mas sim sua lógica."
+                ,"Você pode ter que lutar uma batalha mais de uma vez para vencê-la"
+                ,"É inútil dizer \"estamos a fazer o possível\". Precisamos de fazer o que é necessário."
+                ,"Empresários de sucesso são doadores e não tomadores de energia positiva.","fim"};
+        return desc;
+    }
+
+    public int[] status(){
+        int[] nm={1,2,3};
+        return nm;
+    }
+
+}
